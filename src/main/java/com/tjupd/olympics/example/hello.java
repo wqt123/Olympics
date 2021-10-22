d5368931880c46a6357d89eea96a84281cc078c9
wqt123 <643645865@qq.com>
2021-10-22 22:25:45 +0800

    项目搭建

---
+++ src/main/java/com/tjupd/olympics/example/hello.java
@@ -0,0 +1,7 @@
+package com.tjupd.olympics.example;
+
+public class hello {
+	public void say() {
+		System.out.println("hello word!");
+	}
+}
---
+++ src/test/java/com/tjupd/olympics/testexample/testhello.java
@@ -0,0 +1,12 @@
+package com.tjupd.olympics.testexample;
+import org.junit.Test;
+
+import com.tjupd.olympics.example.hello;
+
+public class testhello {
+	@Test
+	public void testSay() {
+		new hello().say();
+	}
+
+}
