a31f17264db8c89c88166e5382a9c287f89feffa
junjiangyue <1391598941@qq.com>
2021-10-29 22:17:08 +0800

    观众买票、购物测试

---
+++ src/test/java/testTicket/TestAbstractFactoryPattern.java
@@ -0,0 +1,31 @@
+package com.tjupd.olympics.testTicket;
+
+import org.junit.Test;
+import com.tjupd.olympics.abstractFactoryPattern.*;
+
+public class TestAbstractFactoryPattern {
+
+	@Test
+	public void test() {
+		System.out.println("\n抽象工厂模式测试：");
+		AbstractFactory timeFactory = FactoryProducer.getFactory("AdmissionTime");
+		System.out.println("创建入场时间工厂成功！");
+		AdmissionTime time1 = timeFactory.getAdmissionTime("morning");
+	    time1.chooseAdmissionTime();
+	 
+	    AdmissionTime time2 = timeFactory.getAdmissionTime("afternoon");
+	    time2.chooseAdmissionTime();
+	      
+	    AdmissionTime time3 = timeFactory.getAdmissionTime("night");
+	    time3.chooseAdmissionTime();
+	 
+
+	    AbstractFactory purchaseMethodFactory = FactoryProducer.getFactory("PurchaseMethod");
+	    System.out.println("创建售票方式工厂成功！");
+	    PurchaseMethod purchaseMethod1 = purchaseMethodFactory.getPurchaseMethod("OnSiteTicket");
+	    purchaseMethod1.purchaseTicket();
+	 
+	    PurchaseMethod purchaseMethod2 = purchaseMethodFactory.getPurchaseMethod("OnlineTicket");
+	    purchaseMethod2.purchaseTicket();
+	}
+}
