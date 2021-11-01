6a35d3d381e5fb8c79f581ecc45e8c598275d1be
wqt123 <643645865@qq.com>
2021-11-01 17:01:16 +0800

    完善奖牌榜交互

--- src/main/java/com/tjupd/olympics/FrontControllerPattern/AudienceFrontController/MedalTableView.java
+++ src/main/java/com/tjupd/olympics/FrontControllerPattern/AudienceFrontController/MedalTableView.java
@@ -6,25 +6,43 @@ import com.tjupd.olympics.MedalTable.visitor.MedalTableDisplayVisitor;
 import com.tjupd.olympics.MedalTable.visitor.MedalTableGold;
 import com.tjupd.olympics.athletes.Athletes;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class MedalTableView {
     public void show(){
         Athletes athletes = Athletes.getAll();
-        System.out.println("请选择想要查看的奖牌榜类型：");
-        System.out.println("[1]奥运奖牌榜 [2]奥运金牌榜");
         Scanner input = new Scanner(System.in);
-        String option = input.nextLine();
-        if (option == "1") {
-            MedalTable medalTableAll = new MedalTableAll(athletes);
-            medalTableAll.accept(new MedalTableDisplayVisitor());
-        }
-        else if(option == "2"){
-            MedalTable medalTableGold = new MedalTableGold(athletes);
-            medalTableGold.accept(new MedalTableDisplayVisitor());
-        }
-        else {
-            System.out.println("错误的操作码。");
+        boolean flag = true;
+        while (flag){
+            System.out.println("请选择想要查看的奖牌榜类型：");
+            System.out.println("[1]奥运奖牌榜 [2]奥运金牌榜");
+            String option = input.nextLine();
+            if (Objects.equals(option, "1")) {
+                MedalTable medalTableAll = new MedalTableAll(athletes);
+                medalTableAll.accept(new MedalTableDisplayVisitor());
+            }
+            else if(Objects.equals(option, "2")){
+                MedalTable medalTableGold = new MedalTableGold(athletes);
+                medalTableGold.accept(new MedalTableDisplayVisitor());
+            }
+            else {
+                System.out.println("错误的操作码。");
+            }
+            System.out.println("是否希望继续查看奖牌榜");
+            System.out.println("[y]是 [n]否");
+            String tmp = input.nextLine();
+            switch (tmp){
+                case "y":
+                    flag = true;
+                    break;
+                case "n":
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("错误操作码，默认退出！");
+                    flag = false;
+            }
         }
     }
 }
