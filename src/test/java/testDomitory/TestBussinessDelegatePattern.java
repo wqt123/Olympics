7eb3fa6a240991740090a99d3e32232e802bbcb4
wendioooo <yu.wendi@outlook.com>
2021-11-02 22:48:37 +0800

    中文输出

--- src/test/java/testDomitory/TestBussinessDelegatePattern.java
+++ src/test/java/testDomitory/TestBussinessDelegatePattern.java
@@ -23,9 +23,9 @@ public class TestBussinessDelegatePattern {
             String building=domitoryDelegate.getBuilding();
 
             //打印分配结果
-            System.out.println("country"+" "+country);
-            System.out.println("building:"+athlete.getBuilding());
-            System.out.println("roomNumber:"+athlete.getRoomNumber());
+            System.out.println("国家"+" "+country);
+            System.out.println("楼栋号:"+athlete.getBuilding());
+            System.out.println("房间号:"+athlete.getRoomNumber());
             System.out.println();
         }
     }
